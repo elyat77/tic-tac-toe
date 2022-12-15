ae0b2e7483455a7c78b6be40fe22a05f27a3ae60
Elliot <ebedworth@hotmail.co.uk>
2022-12-15 10:51:31 +0000

    Backup from personal

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -4,6 +4,8 @@ import enum
 import re
 from dataclasses import dataclass
 from functools import cached_property
+from tic_tac_toe.logic.exceptions import InvalidMove
+from tic_tac_toe.logic.validators import validate_game_state, validate_grid
 
 WINNING_PATTERNS = (
     "???......",
@@ -16,7 +18,8 @@ WINNING_PATTERNS = (
     "..?.?.?..",
 )
 
-class Mark(enum.StrEnum):
+
+class Mark(str, enum.Enum):
     CROSS = "X"
     NAUGHT = "O"
 
@@ -29,8 +32,7 @@ class Grid:
     cells: str = " " * 9
 
     def __post_init__(self) -> None:
-        if not re.match(r"^[\sXO]{9}$", self.cells):
-            raise ValueError("Must contain 9 cells of: X, O or space")
+        validate_grid(self)
 
     @cached_property
     def x_count(self) -> int:
@@ -55,6 +57,9 @@ class Move:
 class GameState:
     grid: Grid
     starting_mark: Mark = Mark("X")
+
+    def __post_init__(self) -> None:
+        validate_game_state(self)
 
     @cached_property
     def current_mark(self) -> Mark:
@@ -92,3 +97,28 @@ class GameState:
                         match.start()
                         for match in re.finditer(r"\?", pattern)
                     ]
+
+    @cached_property
+    def possible_moves(self) -> list[Move]:
+        moves = []
+        if not self.game_over:
+            for match in re.finditer(r"\s", self.grid.cells):
+                moves.append(self.make_move_to(match.start()))
+        return moves
+
+    def make_move_to(self, index: int) -> Move:
+        if self.grid.cells[index] != " ":
+            raise InvalidMove("Cell is not empty")
+        return Move(
+            mark=self.current_mark,
+            cell_index=index,
+            before_state=self,
+            after_state=GameState(
+                Grid(
+                    self.grid.cells[:index]
+                    + self.current_mark
+                    + self.grid.cells[index + 1:]
+                ),
+                self.starting_mark
+            ),
+        )
